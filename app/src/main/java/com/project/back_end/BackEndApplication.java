9ed48592e83f9af621cfbbc94aefcd01680953b6
mojojojo1234 <panjwanichirag0316@gmail.com>
2026-03-13 10:10:56 +0530

    Completed model classes for Clinic Management System

--- app/src/main/java/com/project/back_end/BackEndApplication.java
+++ app/src/main/java/com/project/back_end/BackEndApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.project.back_end")
+//@ComponentScan("com.project.back_end")
 public class BackEndApplication {
 
 	public static void main(String[] args) {
--- app/src/main/java/com/project/back_end/models/Admin.java
+++ app/src/main/java/com/project/back_end/models/Admin.java
@@ -1,17 +1,24 @@
 package com.project.back_end.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+
 @Entity
 public class Admin {
 
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
 
     @NotNull(message="Username cant be null")
     private String username;
     
     @NotNull(message="Password cant be null")
-    @JsonProperty(access=JsonProperty.Access.WRITE_ONLY)
+    @JsonProperty(access= JsonProperty.Access.WRITE_ONLY)
     private String password;
 
     public Long getId() {
